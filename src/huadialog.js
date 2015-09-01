2c95ff3f0df6ec76dd4c0ac954b1cfecf3642dd4
yuhualinfeng <1135317965@qq.com>
2015-09-01 20:57:31 +0800

    init project

--- src/huadialog.js
+++ src/huadialog.js
@@ -12,8 +12,15 @@
     };
 
     /**
-     * 支持模块输出 requirejs,seajs
+     * 支持模块输出 commonjs
      * */
+    if(define){
+        define(function(require, exports, module) {
+            module.exports=huadialog;
+        });
+    }
+
+
     if (typeof exports !== 'undefined') {
         if (typeof module !== 'undefined' && module.exports) {
             exports = module.exports = huadialog;
@@ -67,7 +74,17 @@
         huadialog.express(options);
 
     };
-    huadialog.prompt = function (string, ok, cancel, defaultValue, isLocked, zIndex) {
+
+    /**
+     * 提示（alert）弹出框
+     * @param string   {String}   提示字符串
+     * @param ok       {String | Function}   确定回调函数
+     * @param cancel   {String | Function}   取消回调函数
+     * @param isLocked {Boolean}  是否显示遮罩层,默认为true
+     * @param zIndex   {Number}   弹层的zIndex值,默认为1024
+     * @return {Boolean}   确定返回true,否则返回false
+     */
+    huadialog.prompt = function (string, ok, cancel, isLocked, zIndex) {
         var options = {};
         if (typeof string === 'object') {
             options = string;
@@ -84,49 +101,51 @@
         huadialog.express(options);
     };
 
+    //默认配置信息
     huadialog.defaultOptions = {
-        //璁剧疆瀵硅瘽妗嗘爣棰�
+        //标题
         title: '消息',
 
-        //璁剧疆瀵硅瘽妗嗗唴瀹�
+        //内容
         content: '&nbsp;',
 
-        //璁剧疆瀵硅瘽妗嗗搴�
+        //宽度
         width: 'auto',
 
-        //璁剧疆瀵硅瘽妗嗛珮搴�
+        //高度
         height: 'auto',
 
-        //璁剧疆瀵硅瘽妗嗚儗鏅�
+        //弹框背景的背景色
         backdropBackground: '#000',
 
-        //璁剧疆瀵硅瘽妗嗚儗鏅�忔槑搴�
-        backgropOpacity: '0.6',
+        //弹框背景的透明度
+        backdropOpacity: '0.3',
 
-        //鏄惁瀵硅瘽妗嗘樉绀鸿儗鏅�
+        //是否显示弹框背景
         locked: false,
 
-        //鐘舵�佹爮淇℃伅
+        //状态栏内容
         statusbar: '',
 
-        //纭鎸夐挳鍥炴帀鍑芥暟
-        ok: null,
+        //确认按钮回调方法
+        //ok: null,
 
-        //鍙栨秷鎸夐挳鍥炴帀鍑芥暟
-        cancel: null,
+        //取消按钮回调方法
+        //cancel: null,
 
-        //纭鎸夐挳鏂囨湰鍊�
+        //确定按钮的值
         okValue: '确定',
 
-        //鍙栨秷鎸夐挳鏂囨湰鍊�
+        //取消按钮的值
         cancelValue: '取消',
 
-        //鏄惁鏄剧ず鍏抽棴鎸夐挳
+        //是否显示关闭按钮
         closeBtn: true,
 
-        //榛樿css鐨剒Index鍊�
+        //css 中 z-index值
         zIndex: 1024,
-        //瀵硅瘽妗嗙粨鏋勪唬鐮�(涓嶅彲閰嶇疆)
+
+        //html结构码
         innderHTML: '<div data-hua="dialog" class="huadialog">' +
         '<table data-hua="dialog-table" class="huadialog-table">' +
         '<tr>' +
@@ -155,38 +174,53 @@
 
     };
     huadialog.express = function (userOptions) {
-        var options = huadialog.defaultOptions, popupBox, backDropBox, center, textInput;
+        var options = huadialog.defaultOptions, popupBox, backDropBox, center, textInput,_$;
+        //对话框居中函数
         center = function () {
             var top = ($(window).height() - popupBox.height()) / 2;
             var left = ($(window).width() - popupBox.width()) / 2;
             popupBox.css({top: top > 0 ? top : 0 + "px", left: left > 0 ? left : 0 + "px"});
         };
+        //hua-dialog对象获取
+        _$=function(ele){
+                return typeof ele =='string' ? popupBox.find('[data-hua=dialog-'+ele+']'):popupBox.find('[data-hua=dialog]');
+        };
+        //弹框对象
         popupBox = $('<div>')
             .css({position: 'absolute', 'z-index': (userOptions.zIndex || 1024) + 1})
             .html(options.innderHTML).appendTo('body');
 
+
         if (userOptions.isLocked !== false) {
+            //弹框背景对象
             backDropBox = $('<div>').addClass('huadialog-backbox').css({'z-index': (userOptions.zIndex || 1024)}).appendTo('body').show();
         }
 
+        //弹框内容填充
         if (userOptions.type !== 'prompt') {
-            popupBox.find('[data-hua=dialog-content]').text(userOptions.string).css({minWidth: '300px'});
+            _$('content').text(userOptions.string).css({minWidth: '300px'});
 
         }
         else {
             textInput = $('<input>').addClass('dialog-prompt-input').val(userOptions.string ? userOptions.string : "");
             var textInputWrap = $('<div>').append(textInput);
-            popupBox.find('[data-hua=dialog-content]').append(textInputWrap).css({minWidth: '300px'});
+            _$('content').append(textInputWrap).css({minWidth: '300px'});
         }
-        popupBox.find('[data-hua=dialog]').addClass('huadialog-show');
-        popupBox.find('[data-hua=dialog-title]').text('提示');
+
+
+        _$().addClass('huadialog-show');
+        _$('title').text('提示');
+
+        //隐藏取消按钮
         if (userOptions.type == 'alert') {
-            popupBox.find('[data-hua=dialog-cancel]').hide();
+            _$('cancel').hide();
         }
         else {
-            popupBox.find('[data-hua=dialog-cancel]').text(options.cancelValue);
+            _$('cancel').text(options.cancelValue);
         }
-        popupBox.find('[data-hua=dialog-ok]').text(options.okValue)
+
+        //确定按钮点击事件
+        _$('ok').text(options.okValue)
             .on('click', function () {
                 if (typeof userOptions.ok == 'function') {
                     userOptions.ok(userOptions.type == 'prompt' ? textInput.val() : false);
@@ -198,26 +232,48 @@
                 popupBox.remove();
                 backDropBox.remove();
             });
+        //取消按钮点击事件
+        _$('cancel').on('click', function () {
+                if (typeof userOptions.cancel == 'function') {
+                    userOptions.cancel();
+                }
+                popupBox.remove();
+                backDropBox.remove();
+            });
 
         center();
+
+        //窗口大小变化事件
         $(window).on('resize', function () {
             center();
         });
+
+        //关闭按钮点击事件
         popupBox.find('[data-hua=dialog-close]').on('click', function () {
             popupBox.remove();
             backDropBox.remove();
         });
     };
 
+    //自定义弹框对象
     huadialog.create = function (options) {
-        var body = $('body');
-        var self = this;
+        var body,self;
+        body = $('body');
+        self=this;
+
+        //获取配置信息
         this.options = options;
+
+        //设置弹框对象
         this.popupBox = $('<div>')
             .css({position: 'absolute', 'z-index': (this.options.zIndex || 1024) + 1})
             .html(options.innderHTML).appendTo(body);
+
+        //设置弹框背景对象
         this.backDropBox = $('<div>').addClass('huadialog-backbox').css({'z-index': (this.options.zIndex || 1024)})
             .appendTo(body);
+
+        //定义事件列表
         this.event = {
             show: [],
             close: [],
@@ -225,23 +281,32 @@
             okClick: [],
             cancelClick: []
         };
+
+        //读取并使用配置信息
         $.each(options, function (key, value) {
             if (typeof self[key] === 'function') {
                 self[key](value);
             }
         });
 
+        //定义关闭按钮点击事件
         this._$('dialog-close').on('click', function () {
             self.close().remove();
         });
+
+        //定义确定按钮点击事件
         this._$('dialog-ok').on('click', function () {
             if (self.triggerEvent('okClick') !== false)
                 self.close().remove();
         });
+
+        //定义取消按钮点击事件
         this._$('dialog-cancel').on('click', function () {
             if (self.triggerEvent('cancelClick') !== false)
                 self.close().remove();
         });
+
+        //定义窗口大小改变事件
         $(window).on('resize', function () {
             self.center();
         });
@@ -249,6 +314,7 @@
     var prototype = huadialog.create.prototype;
     $.extend(prototype, {
 
+        //显示弹框
         show: function () {
             this.center();
             this.popupBox.show();
@@ -257,15 +323,17 @@
             }
             this._$('dialog').addClass('huadialog-show');
             this.triggerEvent('show');
-
             return this;
         },
+
+        //设置弹框标题
         title: function (title) {
             if (typeof title === 'string') {
                 this._$('dialog-title').text(title);
             }
         },
 
+        //设置弹框内容,可以为字符串,也可以为html对象
         content: function (html) {
             if (typeof html === 'string')
                 this._$('dialog-content').html(html);
@@ -273,15 +341,20 @@
                 this._$('dialog-content').empty().append(html);
             }
         },
+
+        //设置弹框宽度
         width: function (width) {
             this._$('dialog-content').width(width);
             return this;
         },
 
+        //设置弹框高度
         height: function (height) {
             this._$('dialog-content').height(height);
             return this;
         },
+
+        //设置状态信息,可以为字符串,也可以为html对象
         statusbar: function (html) {
             if (typeof html === 'string')
                 this._$('dialog-status').html(html);
@@ -289,32 +362,44 @@
                 this._$('dialog-status').empty().append(html);
             }
         },
+
+        //设置弹框背景的背景色
         backdropBackground: function (color) {
             this.backDropBox.css({background: color});
             return this;
         },
+
+        //设置弹框背景的透明度
         backdropOpacity: function (opacity) {
             var ie = "alpha(opacity=" + parseInt(opacity * 100) + ")";
             this.backDropBox.css({opacity: opacity, filter: ie});
             return this;
         },
+
+        //设置是否显示关闭按钮
         closeBtn: function (value) {
             if (value) {
-                this._$('dialog-close').hide();
+                this._$('dialog-close').show();
             }
             else {
-                this._$('dialog-close').show();
+                this._$('dialog-close').hide();
             }
             return this;
         },
+
+        //为确定按钮添加事件监听
         ok: function (fn) {
             this.addEventListener('okClick', fn);
             return this;
         },
+
+        //为取消按钮添加事件监听
         cancel: function (fn) {
             this.addEventListener('cancelClick', fn);
             return this;
         },
+
+        //设置确定按钮的值
         okValue: function (value) {
             if (typeof value === 'string') {
                 this._$('dialog-ok').text(value);
@@ -324,6 +409,8 @@
             }
             return this;
         },
+
+        //设置取消按钮的值
         cancelValue: function (value) {
             if (typeof value === 'string') {
                 this._$('dialog-cancel').text(value);
@@ -334,32 +421,43 @@
             return this;
         },
 
+        //获取huadialog对象
         _$: function (value) {
             return this.popupBox.find('[data-hua=' + value + ']');
         },
+
+        //关闭弹框
         close: function () {
             this.popupBox.hide();
             this.backDropBox.hide();
             this.triggerEvent('close');
             return this;
         },
+
+        //移除弹框
         remove: function () {
             this.popupBox.remove();
             this.backDropBox.remove();
             this.triggerEvent('remove');
             return this;
         },
+
+        //弹框居中
         center: function () {
             var top = ($(window).height() - this.popupBox.height()) / 2;
             var left = ($(window).width() - this.popupBox.width()) / 2;
             this.popupBox.css({top: top > 0 ? top : 0 + "px", left: left > 0 ? left : 0 + "px"});
             return this;
         },
+
+        //添加自定义事件
         addEventListener: function (name, fn) {
             if (this.event[name] && $.isArray(this.event[name])) {
                 this.event[name].push(fn);
             }
         },
+
+        //触发自定义事件
         triggerEvent: function (name) {
             var reValue;
             $.each(this.event[name], function (key, value) {
